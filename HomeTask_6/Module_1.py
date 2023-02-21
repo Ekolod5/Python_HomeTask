8aa4ff11fcb7f59329b6e80d9c8e019e69d1dcc8
lena <ekolod5@mail.ru>
2023-02-21 19:18:18 +0300

    Module_1

---
+++ HomeTask_6/Module_1.py
@@ -0,0 +1,21 @@
+
+def arithmetic_prog(a1: int, diff: int, n:int):
+    sequence = [a1]
+    for i in range(2, n + 1):
+        sequence.append(a1 + (i - 1) * diff)
+    return sequence
+
+
+def create_list(x: int):
+    list_1 = []
+    for _ in range(x):
+        list_1.append(int(input('Введите число: ')))
+    return list_1
+
+
+def indexes_array(num_list: list, start: int, end: int):
+    indexes_list = []
+    for i in range(len(num_list)):
+        if start <= num_list[i] <= end:
+            indexes_list.append(i)
+    return indexes_list
